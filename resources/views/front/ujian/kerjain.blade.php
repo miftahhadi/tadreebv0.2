0877b580de74f3fca9204e92415c46f780062022
turobi <miftahhadi@gmail.com>
2020-06-28 08:33:37 +0700

    Export $start dan $end dengan json_encode

--- resources/views/front/ujian/kerjain.blade.php
+++ resources/views/front/ujian/kerjain.blade.php
@@ -16,8 +16,8 @@
 
                     <div class="card-action ml-auto">
                         <timer 
-                            starttime="{{ $start }}"
-                            endtime="{{ $end }}"
+                            starttime="{{ json_encode($start) }}"
+                            endtime="{{ json_encode($end) }}"
                             ></timer>
                     </div>
                 </div>
